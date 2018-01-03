908208f0dc8d4e2f0145485b7379448e8e3beacb
Nathan Phelps <nathan@codekoalas.com>
2018-01-02 22:22:09 -0600

    Quotes pulling, have had trouble getting more than one quote Using new API, but only a Quote of the Day Fun with Fetch Src

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,30 @@
 document.querySelector('#load-quote').addEventListener('click', () => {
-  alert('Hi!');
+  const quoteHeader = new Headers({
+    method: 'GET',
+    'Content-Type': 'application/json'
+  });
+  const quote = fetch('http://quotes.rest/qod.json');
+  // ?filter[orderby]=rand&filter[posts_per_page]=1
+  quote
+    .then(response => {
+      var contentType = response.headers.get('content-type');
+      if (contentType && contentType.includes('application/json')) {
+        return response.json();
+      } else {
+        throw new TypeError("That's no JSON!");
+      }
+    })
+    .then(data => {
+      const quoteData = data.contents.quotes[0];
+      document.getElementById('quote-display').innerHTML = `${quoteData.quote}`;
+      document.getElementById('author-title').innerHTML = `- ${
+        quoteData.author
+      },<br> &nbsp; &nbsp;${quoteData.title}`;
+    })
+    .catch(error => {
+      alert(error);
+      console.log(error);
+    });
 });
 
 // http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en
